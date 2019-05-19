9f7a08761678806af9a92ff4bc6a49957603eca4
Defrianda05 <50765781+Defrianda05@users.noreply.github.com>
2019-05-19 21:52:36 +0800

    Inputan menggunakan perulangan
    Inputan menggunakan perulangan

---
+++ Algoritma input.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+int main ()
+{
+	int absen;
+	int tugas;
+	int mid;
+	int final;
+	int hasil;
+	int i;
+	int a;
+	char nama[100];
+	cout << "Masukan Jumlah Inputan : ";cin>>a;
+	for (i=1; i<=a;i++) {
+	cout << "masukan nama :";gets(nama);cin.getline(nama,100);
+	cout << "Nilai absen : ";cin>>absen;
+	cout << "Nilai tugas : ";cin>>tugas;
+	cout << "Nilai mid : ";cin>>mid;
+	cout << "Nilai final : ";cin>>final; 
+	hasil = (absen*0.1) + (tugas*0.2) + (mid*0.3) + (final*0.4);
+	cout << "hasil " <<hasil<<"\n";
+	if (hasil >= 81 ) {
+		cout << "Grade A \n";
+	} else if (hasil >=71) {
+		cout << "Grade B \n";
+	} else if (hasil >= 60) {
+		cout << "Grade C \n";
+	} else if (hasil >= 50) {
+		cout << "Grade D \n";
+	} else {
+		cout << "Grade E \n";
+	}
+}
+
+}
+
+
+
+
+
+
+
+
